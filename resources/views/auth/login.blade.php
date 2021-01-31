2adf05a807c8bdb5406b7de074b1c1f8af846220
rifanipermata <rifanipermata102@gmail.com>
2021-02-01 00:13:46 +0700

    buat fungsi login berjalan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,11 +18,20 @@
           <form action="{{ route('login') }}" method="post">
             @csrf
             <div class="input-group mb-3">
-              <input type="email" class="form-control" placeholder="username">
+              <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus placeholder="username">
+              @error('username')
+                <span class="invalid-feedback bg-white text-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+              @enderror
             </div>
             <div class="input-group mb-3">
-              <input type="password" class="form-control" placeholder="password" >
-              
+              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+              @error('password')
+                <span class="invalid-feedback bg-white text-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+              @enderror
             </div>
             <div class="row">
                 <div class="col mt-2">
