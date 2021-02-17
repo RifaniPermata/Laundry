4324632093dcaa03d8c49c682b9ed99c3431ec38
rifanipermata <rifanipermata102@gmail.com>
2021-02-17 23:25:55 +0700

    buat history login user

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Models\Transaksi');
     }
+
+    public function logLogins()
+    {
+        return $this->hasMany('App\Models\LogLogin');
+    }
 }
